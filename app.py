8fc993ac7edb02598d87a8c1404fd471c56056b8
Daniel Cook <danielecook@gmail.com>
2017-04-30 20:07:38 -0500

    cleanup api, integrate; pep8

--- app.py
+++ app.py
@@ -6,19 +6,21 @@ app = Flask(__name__, static_url_path='/static')
 def root():
     return app.send_static_file('index.html')
 
+
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('static/js', path)
 
+
 @app.route('/css/<path:path>')
 def send_css(path):
     return send_from_directory('static/css', path)
+
 
 @app.route('/data/<path:path>')
 def send_data(path):
     return send_from_directory('static/data', path)
 
 
-
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
