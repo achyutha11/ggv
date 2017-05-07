45b113d12b3848ddbd3527eb275d05421017abf6
= <=>
2017-05-07 13:07:58 -0500

    update api

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-from flask import Flask, render_template, send_from_directory
-app = Flask(__name__, static_url_path='/static')
+#!/usr/env/bin python
+#from api import FreqVcfTable
+from flask import Flask, send_from_directory, request
+from flask.ext.restful import Api
+from flask.ext.restful.utils import cors
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+
+app = Flask(__name__, static_folder = 'static')
+app.debug = True
+handler = RotatingFileHandler('/var/www/dev-integrated/ggv.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
+app.config["APPLICATION_ROOT"] = "/dev-integrated"
 
 
 @app.route('/')
@@ -7,20 +21,24 @@ def root():
     return app.send_static_file('index.html')
 
 
-@app.route('/js/<path:path>')
-def send_js(path):
-    return send_from_directory('static/js', path)
-
-
-@app.route('/css/<path:path>')
-def send_css(path):
-    return send_from_directory('static/css', path)
-
-
 @app.route('/data/<path:path>')
-def send_data(path):
-    return send_from_directory('static/data', path)
+@app.route('/css/<path:path>')
+@app.route('/js/<path:path>')
+def send_static(path):
+    app.logger.info("GREAT!")
+    app.logger.info(os.path.join('static', request.path))
+    return send_from_directory(os.path.join(app.static_folder, request.path), path)
+
+@app.route("/test")
+def test():
+    return "great!"
+
+# Set up API resources
+#api.add_resource(FreqVcfTable, '/freq_table')
+# Basic header info
+#api = Api(app)
+#api.decorators = [cors.crossdomain(origin='*')]
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- app.wsgi
+++ app.wsgi
@@ -1,5 +1,11 @@
 import sys
 import os
+import logging
+
+logging.basicConfig(stream=sys.stderr)
+
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
 from app import app as application
+from werkzeug.debug import DebuggedApplication 
+application = DebuggedApplication(application, True)
