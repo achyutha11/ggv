d78d4ad0299db2d02c9a8595ef41fba7d0af9dc1
Daniel Cook <danielecook@gmail.com>
2018-08-13 09:15:10 -0500

    disable log issue

--- main.py
+++ main.py
@@ -24,19 +24,19 @@ from flask import Flask
 import logzero
 
 
-from flask.logging import default_handler
+#from flask.logging import default_handler
 
-class RequestFormatter(logging.Formatter):
-    def format(self, record):
-        record.url = request.url
-        record.remote_addr = request.remote_addr
-        return super().format(record)
+# class RequestFormatter(logging.Formatter):
+#     def format(self, record):
+#         record.url = request.url
+#         record.remote_addr = request.remote_addr
+#         return super().format(record)
 
-formatter = RequestFormatter(
-    '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
-    '%(levelname)s in %(module)s: %(message)s'
-)
-default_handler.setFormatter(formatter)
+# formatter = RequestFormatter(
+#     '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
+#     '%(levelname)s in %(module)s: %(message)s'
+# )
+# default_handler.setFormatter(formatter)
 
 app = Flask(__name__)
 
