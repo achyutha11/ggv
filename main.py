68e4623a7a0ad1b549b407a82cc717d6b77ee7fd
Daniel Cook <danielecook@gmail.com>
2018-08-13 09:08:18 -0500

    Fix login

--- main.py
+++ main.py
@@ -21,6 +21,23 @@ from flask_compress import Compress
 from flask_cors import CORS, cross_origin
 from flask import Flask
 
+import logzero
+
+
+from flask.logging import default_handler
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        record.url = request.url
+        record.remote_addr = request.remote_addr
+        return super().format(record)
+
+formatter = RequestFormatter(
+    '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
+    '%(levelname)s in %(module)s: %(message)s'
+)
+default_handler.setFormatter(formatter)
+
 app = Flask(__name__)
 
 
