48bf46fd71f357e31c0668197424e7a5c9d50341
Achyutha Ram Menon <achyutha11@midway2-login1.rcc.local>
2021-02-27 08:01:43 -0600

    Changed app route

--- api/hello.py
+++ api/hello.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route("/api/ld/<string:dataset>/<string:query>", methods=['GET'])
+def find_ld():
     return "Hello world!"
